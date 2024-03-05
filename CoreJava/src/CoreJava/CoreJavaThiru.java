42d71e836a2c4e410d7f679b7f2d244285457b10
HP <HP@DESKTOP-QRO2UF8>
2024-03-05 16:27:06 +0530

    Java

---
+++ CoreJava/src/CoreJava/CoreJavaThiru.java
@@ -0,0 +1,18 @@
+package CoreJava;
+
+import java.util.Scanner;
+
+public class CoreJavaThiru {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter The Value Of A and B");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("The Sum Of "+a+" and "+b+" Is"+c);
+
+	}
+
+}
