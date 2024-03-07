6adbd244647599443f0e855404a7f9ea8b2cf78c
HP <HP@DESKTOP-QRO2UF8>
2024-03-07 13:50:56 +0530

    d

---
+++ CoreJava/src/day3/Customer.java
@@ -0,0 +1,28 @@
+package day3;
+
+public class Customer {
+	private int cid;
+	private String name;
+	private String address;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+														
+
+}
---
+++ CoreJava/src/day3/Exe.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class Exe {
+	public static void main(String[] args) {
+		Customer cl=new Customer();
+		cl.setAddress("TamilNadu");
+		cl.setCid(10);
+		cl.setName("puvi");
+		System.out.println("ID: "+cl.getCid()+"\nName: "+cl.getName()+"\nADDRESS: "+cl.getAddress());
+		
+	}
+	
+
+}
