516e067fdd63e17dd8dd73e73cee3ad865f2f0d4
HP <HP@DESKTOP-QRO2UF8>
2024-03-07 14:01:37 +0530

    df

--- CoreJava/src/day3/Exe.java
+++ CoreJava/src/day3/Exe.java
@@ -1,14 +1,17 @@
 package day3;
 
+
 public class Exe {
 	public static void main(String[] args) {
 		Customer cl=new Customer();
 		cl.setAddress("TamilNadu");
 		cl.setCid(10);
-		cl.setName("puvi");
+		cl.setName("Puvi");
 		System.out.println("ID: "+cl.getCid()+"\nName: "+cl.getName()+"\nADDRESS: "+cl.getAddress());
 		
 	}
 	
 
 }
+
+
